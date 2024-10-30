f39f5dd2940beadef3a3ba16a7c34a3ce709b0ff
Lafuente Paulino <lafuente12erty@gmail.com>
2024-10-30 13:19:08 -0300

    initial commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studying_DotNet.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void apresentar(){
+                Console.WriteLine($"Olá, meu nome é {Nome} eu tenho {Idade} anos");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Studying_DotNet.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "João";
+pessoa1.Idade = 10;
+
+pessoa1.apresentar();
